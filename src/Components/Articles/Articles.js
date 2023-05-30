ffdafb1df905441c0526e5d77c6ede3f398fe6b5
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-30 17:02:43 -0600

    Created API Call, Mock data and Article.js

---
+++ src/Components/Articles/Articles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Articles.css'
+
+function Articles() {
+  return (
+    <div>
+        <section className='article-section'>
+            <div className='article-box'></div>
+            <div className='article-box'></div>
+        </section>
+    </div>
+  )
+}
+
+export default Articles
