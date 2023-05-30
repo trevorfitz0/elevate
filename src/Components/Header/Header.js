5d4c27364b1ab3adaacee4d170f8dcfdec081f81
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-30 13:45:03 -0600

    Header and Color Variable creation

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='head'>
+        <h1 className='title'>ELEVATE</h1>
+    </div>
+  )
+}
+
+export default Header
