d76a9ba61ab5f366ddc5ccf767ead22cba6e4a76
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-30 19:07:44 -0600

    Created page for single article

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,8 @@ import { getNews } from '../../APICalls';
 import Header from '../Header/Header';
 import getSampleData from '../../SampleData';
 import Articles from '../Articles/Articles';
+import { Route, Switch } from 'react-router-dom';
+import Article from '../Article/Article';
 
 export default class App extends Component {
 
@@ -26,7 +28,14 @@ export default class App extends Component {
     return (
       <div>
         <Header/>
-        <Articles articles={ this.state.articles }/>
+        <Switch>
+          <Route exact path="/" render={() => <Articles articles={ this.state.articles }/>}/>
+          <Route exact path='/article/:title' render={({ match }) => {
+          const article = this.state.articles.find(article => article.title.replace(/\s+/g, '') === match.params.title)
+          console.log(article)
+          return <Article article={ article } />
+        }}/>
+        </Switch>
       </div>
     )
   }
