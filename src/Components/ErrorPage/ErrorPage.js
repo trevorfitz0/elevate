0776e3111aec4691b870590d8ab6952616c3aa2c
trevorfitz0 <trevorfitz0@gmail.com>
2023-06-01 13:08:56 -0600

    Fixed Underlining for buttons and added element to the error handling page

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -6,10 +6,9 @@ function ErrorPage() {
   return (
     <div className='error-page'>
         <div className='page-content'>
-            <h1>Page not found</h1>
-            <NavLink to={`/`}>
-                        <h2 className='home-button'>Back Home</h2>
-            </NavLink>
+            <h1 className='error-text'>Page not found</h1>
+            <i class="fa-solid fa-bomb fa-bounce fa-2xl"></i>
+            <NavLink className='link' to={`/`}>Back Home</NavLink>
         </div>
     </div>
   )
